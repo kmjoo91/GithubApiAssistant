df81a3f415961781e6ae5978bddfa9a923f0d60e
kim.minjoo <kim.minjoo@nts-corp.com>
2020-02-13 21:32:41 +0900

    create project

---
+++ src/main/java/me/kmjoo91/githubapiassistant/GithubApiAssistantApplication.java
@@ -0,0 +1,13 @@
+package me.kmjoo91.githubapiassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApiAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubApiAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/kmjoo91/githubapiassistant/GithubApiAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kmjoo91.githubapiassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApiAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
